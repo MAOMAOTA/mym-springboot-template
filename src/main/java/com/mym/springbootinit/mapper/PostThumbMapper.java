482553dc58e7a589029911940dcbdab8f0dea1cd
MAOMAOTA <2220324086@qq.com>
2025-04-24 22:31:37 +0800

    [cu]:init commit

---
+++ src/main/java/com/mym/springbootinit/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.mym.springbootinit.mapper;
+
+import com.mym.springbootinit.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ *
+ *
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
